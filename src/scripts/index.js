7a78ccf0a59d6491daa80108c840bb0a40d7b716
Guilherme Nicolini <guilhermenicolini1@gmail.com>
2022-11-22 11:44:08 -0300

    Adionado a pasta objeto projeto

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,16 @@
-import{user} from '/src/scripts/services/user.js'
-import{repositories} from '/src/scripts/services/repositories.js'
+import { getUser } from '/src/scripts/services/user.js'
+
+import { getRepositories } from '/src/scripts/services/repositories.js'
+
+import { user } from '/src/scripts/objects/user.js'
+
+import { screen } from '/src/scripts/objects/screen.js'
+
 
 document.getElementById("btn-search").addEventListener("click", () => {
     const userName = document.getElementById("input-search").value
-    getUserProfile(userName)
+   
+    getUserData(userName)
 })
 
 document.getElementById("input-search").addEventListener("keyup", (e) => {
@@ -12,40 +19,23 @@ document.getElementById("input-search").addEventListener("keyup", (e) => {
     const isEnterKeyPressed = key === 13
 
     if (isEnterKeyPressed) {
-        getUserProfile(userName)
+        getUserData(userName)
     }
+
 })
 
-function getUserProfile(userName) {
-    user(userName).then(userData => {
 
-        let userInfo = `<div class="info">
-                         <img src="${userData.avatar_url}" alt="foto do perfil"/>
-                         <div class="data"> 
-                           <h1>${userData.name ?? "Não possui nome cadastrado 😒"} </h1>
-                           <p>${userData.bio ?? "Não possui bio cadastrado 😒"}</p>
-                          
-                           </div>
-                          </div>`
 
-        document.querySelector(".profile-data").innerHTML = userInfo
+async function getUserData(userName) {
+    const userResponse = await getUser(userName)
 
-        getUserRepositories(userName)
-    })
+    const repositoriesResponse = await getRepositories(userName)
+
+    user.setInfo(userResponse)
+    user.setRepositories(repositoriesResponse)
+
+    screen.renderUser(user)
 }
 
-function getUserRepositories(userName) {
-    repositories(userName).then(resposData => {
-        let repositoriesItens = ""
-        resposData.forEach(repo => {
-            repositoriesItens += `<li><a href= "${repo.html_url}" target="_blank">${repo.name}</a></li>`
-        })
 
-        document.querySelector(".profile-data").innerHTML +=
-            ` <div class="repositories section"> 
-          <h2>Repositórios</h2>
-          <ul>${repositoriesItens}</ul>
-         </div>`
-    })
-}
 
